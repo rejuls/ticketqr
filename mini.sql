0a9fd143aad808af3f77e1e91e9b88498041c27e
RejulS <s.rejul10@gmail.com>
2018-11-11 17:45:48 +0530

    .

--- mini.sql
+++ mini.sql
@@ -6,12 +6,9 @@ create database online_booking;
                    password varchar(20) not null,
                    email varchar(30) not null,
                    phone int not null,
-                   credit int DEFAULT '50');
+                   credit int DEFAULT '500');
  create table bus(bus_no varchar(20) not null primary key,
-                  name varchar(20) not null,
-                  tot_seat int not null,
-                  avail_seat int,
-                  seat_pos varchar(5));
+                  name varchar(20) not null;
 create table stand( stand_code varchar(20) not null primary key,
                     name varchar(20) not null,
                     bus_no varchar(20),
@@ -20,12 +17,14 @@ create table journey(journey_code varchar(20) not null primary key,
                      date date not null,
                      start_time time not null,
                      reach_time time not null,
+                     tot_seat int not null,
+                     avail_seat int,
                      bus_no varchar(20),
                      foreign key(bus_no) references bus(bus_no),
                      start_stand varchar(20),
                      foreign key(start_stand) references stand(stand_code),
                      end_stand varchar(20),
-                     foreign key(end_stand) references stand(stand_code)); 
+                     foreign key(end_stand) references stand(stand_code));
 create table ticket( ticket_id int not null primary key AUTO_INCREMENT,
                      cost int not null,
                      id int,
@@ -36,8 +35,7 @@ create table train( train_no varchar(20) not null primary key,
                   name varchar(20) not null,
                   bogy_no varchar(5) not null,
                   tot_seat int not null,
-                  avail_seat int,
-                  seat_pos varchar(5));
+                  avail_seat int,;
 create table station( station_code varchar(20) not null primary key,
                     name varchar(20) not null,
                     train_no varchar(20),
@@ -51,8 +49,12 @@ create table travel(travel_code varchar(20) not null primary key,
                      start_station varchar(20),
                      foreign key(start_station) references station(station_code),
                      end_station varchar(20),
-                     foreign key(end_station) references station(station_code)); 
+                     foreign key(end_station) references station(station_code));
 create table passenger( id int not null primary key AUTO_INCREMENT,
                         age int not null,
-                        name varchar(20) not null); 
-    
+                        name varchar(20) not null);
+create table message( id int not null primary key AUTO_INCREMENT,
+			name varchar(20) not null,
+			email varchar(30) not null,
+      phone int not null,
+			message longtext not null);
